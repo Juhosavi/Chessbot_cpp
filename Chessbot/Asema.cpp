979bbad161aff3acd9a311720a4a71637642576b
Juhosavi <101104622+Juhosavi@users.noreply.github.com>
2025-01-17 11:09:53 +0200

    working grid

--- Chessbot/Asema.cpp
+++ Chessbot/Asema.cpp
@@ -3,18 +3,37 @@
 
 void Asema::tulosta() const
 {
-    for (int j = 0; j < 9; j++)
+    for (int rivi = 0; rivi < 8; rivi++)
     {
-        for (int i = 0; i < 8; i++)
+        for (int linja = 0; linja < 8; linja++)
         {
             std::cout << "+---";
         }
 
         std::cout << "+" << std::endl;
-        for (int k = 0; k < 8; k++)
+        for (int linja = 0; linja < 8; linja++)
         {
             std::cout << "|";
-            std::cout << " x ";
+            int nappula = _lauta[rivi][linja];
+            switch (nappula)
+            {
+            case wR: std::cout << " R "; break;
+            case wN: std::cout << " N "; break;
+            case wB: std::cout << " B "; break;
+            case wQ: std::cout << " Q "; break;
+            case wK: std::cout << " K "; break;
+            case wP: std::cout << " P "; break;
+            case bR: std::cout << " r "; break;
+            case bN: std::cout << " n "; break;
+            case bB: std::cout << " b "; break;
+            case bQ: std::cout << " q "; break;
+            case bK: std::cout << " k "; break;
+            case bP: std::cout << " p "; break;
+            default: std::cout << "   "; break;
+
+
+
+            }
         }
  
 
@@ -30,5 +49,5 @@ void Asema::tulosta() const
 
 void Asema::tyhjenna()
 {
-
+    
 }
