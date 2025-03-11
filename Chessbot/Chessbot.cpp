a39eba7f4338915594e04004ca2c8400be76728e
Juho Savinainen <savinainen.juho@gmail.com>
2025-03-11 12:39:30 +0200

    asynckokeilu

--- Chessbot/Chessbot.cpp
+++ Chessbot/Chessbot.cpp
@@ -1,21 +1,48 @@
 #include <iostream>
-#include <vector>
 #include <thread>
-#include <map>
-#include <SFML/Graphics.hpp>
 #include "asema.h"
 #include "siirto.h"
 #include "shakki.h"
-#include <execution>
 #include "kayttis.h"
 
+using namespace std;
 
 int main() {
+    srand(static_cast<unsigned>(time(0)));
     Asema asema;
-    // Luodaan säikeet graafiselle ja tekstipohjaiselle käyttöliittymälle
-    thread t1(sfml_gui, ref(asema));
-    thread t2(terminal_ui, ref(asema));
-    t1.join();
-    t2.join();
+    int interfaceChoice, colorChoice;
+
+    cout << "Valitse käyttöliittymä:" << endl;
+    cout << "  1. SFML" << endl;
+    cout << "  2. Terminal" << endl;
+    cout << "  3. Molemmat (SFML ja Terminal)" << endl;
+    cout << "Valintasi: ";
+    cin >> interfaceChoice;
+
+    cout << "Valitse pelattava väri:" << endl;
+    cout << "  1. Valkoinen (pelaat manuaalisesti)" << endl;
+    cout << "  2. Musta (pelaat manuaalisesti)" << endl;
+    cout << "Valintasi: ";
+    cin >> colorChoice;
+
+    int playerColor = (colorChoice == 1) ? VALKEA : MUSTA;
+
+    // Pelaajan ohjaama puoli toimii manuaalisesti; toinen puoli on AI.
+    if (interfaceChoice == 1) {
+        sfml_gui(asema, playerColor);
+    }
+    else if (interfaceChoice == 2) {
+        terminal_ui(asema, playerColor);
+    }
+    else if (interfaceChoice == 3) {
+        thread t1(sfml_gui, ref(asema), playerColor);
+        thread t2(terminal_ui, ref(asema), playerColor);
+        t1.join();
+        t2.join();
+    }
+    else {
+        cout << "Virheellinen valinta!" << endl;
+    }
+
     return 0;
 }
--- Chessbot/kayttis.h
+++ Chessbot/kayttis.h
@@ -8,6 +8,9 @@
 #include "siirto.h"
 #include "shakki.h"
 #include <execution>
+#include <limits>
+#include <cstdlib>
+#include <ctime>
 
 using namespace std;
 
@@ -17,7 +20,7 @@ const int tileSize = 100;
 // Tekstuurit kaikille nappuloille
 map<string, sf::Texture> textures;
 
-// Lataa kaikki shakkipelin nappulat
+// Lataa kaikki shakkipelin nappuloiden tekstuurit
 void lataaTekstuurit() {
     vector<string> pieces = {
         "black-bishop", "black-king", "black-knight", "black-pawn",
@@ -34,19 +37,18 @@ void lataaTekstuurit() {
     }
 }
 
-// Näytä ylennysvalinta SFML-ikkunassa ja anna käyttäjän valinta
-// Tämä funktio odottaa, että käyttäjä klikkaa jonkin valinnan alueen.
+// Näytä ylennysvalinta SFML-ikkunassa ja anna käyttäjän tehdä valinta
 void promotionDialog(sf::RenderWindow& window, Asema& asema, Siirto& move) {
     int chosenPromotion = NA;
     bool validSelection = false;
 
-    // Luodaan promotion-valinnan sprite-oliot.
+    // Luodaan sprite-oliot ylennysvalinnalle.
     sf::Sprite queenSprite(textures["white-queen"]);
     sf::Sprite rookSprite(textures["white-rook"]);
     sf::Sprite bishopSprite(textures["white-bishop"]);
     sf::Sprite knightSprite(textures["white-knight"]);
 
-    // Aseta spritejen sijainnit (esim. ruudut keskelle ikkunaa)
+    // Aseta spritejen sijainnit (keskelle ikkunaa)
     queenSprite.setPosition(300, 300);
     rookSprite.setPosition(450, 300);
     bishopSprite.setPosition(300, 450);
@@ -75,7 +77,6 @@ void promotionDialog(sf::RenderWindow& window, Asema& asema, Siirto& move) {
                 }
             }
         }
-
         // Piirretään ylennysvalinta-overlay
         window.clear();
         sf::RectangleShape overlay(sf::Vector2f(window.getSize().x, window.getSize().y));
@@ -92,8 +93,8 @@ void promotionDialog(sf::RenderWindow& window, Asema& asema, Siirto& move) {
     move._korotettava_nappula = chosenPromotion;
 }
 
-// SFML-käyttöliittymä, joka piirtää shakkilaudan ja käsittelee hiiritapahtumat
-void sfml_gui(Asema& asema) {
+// SFML-käyttöliittymä, jossa pelaajan vuorolla odotetaan hiiritapahtumia ja muulloin AI laskee siirtonsa.
+void sfml_gui(Asema& asema, int playerColor) {
     sf::Font font;
     if (!font.loadFromFile("C:/Users/savin/source/repos/Chessbot/font/AldotheApache.ttf")) {
         cerr << "Virhe ladattaessa fonttia!" << endl;
@@ -116,6 +117,7 @@ void sfml_gui(Asema& asema) {
         {bB, "black-bishop"}, {bQ, "black-queen"}, {bK, "black-king"}
     };
 
+    // Muuttujat pelaajan siirron hallintaan
     vector<Siirto> mahdollisetSiirrot;
     mahdollisetSiirrot.reserve(100);
     int valittuRivi = -1, valittuLinja = -1;
@@ -128,55 +130,56 @@ void sfml_gui(Asema& asema) {
             if (event.type == sf::Event::Closed)
                 window.close();
 
-            if (event.type == sf::Event::MouseButtonPressed && event.mouseButton.button == sf::Mouse::Left) {
-                int x = (event.mouseButton.x - 50) / tileSize;
-                int y = (event.mouseButton.y - 50) / tileSize;
-                if (x >= 0 && x < 8 && y >= 0 && y < 8) {
-                    if (!siirtoValittu) {
-                        valittuRivi = y;
-                        valittuLinja = x;
-                        mahdollisetSiirrot.clear();
-                        asema.anna_siirrot(mahdollisetSiirrot);
-                        siirtoValittu = true;
-                    }
-                    else {
-                        bool validMove = false;
-                        for (const auto& s : mahdollisetSiirrot) {
-                            if (s._a_r == valittuRivi && s._a_l == valittuLinja &&
-                                s._l_r == y && s._l_l == x) {
-                                valittuSiirto = s;
-                                // Tarkistetaan, onko kyseessä ylennystilanne (sotilas päässyt viimeiselle riville)
-                                if ((asema._siirtovuoro == VALKEA && y == 0 && asema._lauta[valittuRivi][valittuLinja] == wP) ||
-                                    (asema._siirtovuoro == MUSTA && y == 7 && asema._lauta[valittuRivi][valittuLinja] == bP)) {
-                                    promotionDialog(window, asema, valittuSiirto);
-                                }
-                                asema.tee_siirto(valittuSiirto, asema._siirtovuoro);
-                                validMove = true;
-                                break;
-                            }
+            // Jos on pelaajan vuoro (valitun värin mukaisesti), käsitellään hiiritapahtumat.
+            if (asema._siirtovuoro == playerColor) {
+                if (event.type == sf::Event::MouseButtonPressed && event.mouseButton.button == sf::Mouse::Left) {
+                    int x = (event.mouseButton.x - 50) / tileSize;
+                    int y = (event.mouseButton.y - 50) / tileSize;
+                    if (x >= 0 && x < 8 && y >= 0 && y < 8) {
+                        if (!siirtoValittu) {
+                            // Ensimmäisellä klikkauksella valitaan lähtöruutu
+                            valittuRivi = y;
+                            valittuLinja = x;
+                            mahdollisetSiirrot.clear();
+                            asema.anna_siirrot(mahdollisetSiirrot);
+                            siirtoValittu = true;
                         }
-                        if (!validMove)
-                            siirtoValittu = false;
+                        else {
+                            // Toisella klikkauksella etsitään siirto, joka lähtee valitusta ruudusta.
+                            bool validMove = false;
+                            for (const auto& s : mahdollisetSiirrot) {
+                                if (s._a_r == valittuRivi && s._a_l == valittuLinja &&
+                                    s._l_r == y && s._l_l == x) {
+                                    valittuSiirto = s;
+                                    // Tarkistetaan ylennystilanne (esim. sotilas pääsee viimeiselle riville)
+                                    if ((asema._siirtovuoro == VALKEA && y == 0 && asema._lauta[valittuRivi][valittuLinja] == wP) ||
+                                        (asema._siirtovuoro == MUSTA && y == 7 && asema._lauta[valittuRivi][valittuLinja] == bP)) {
+                                        promotionDialog(window, asema, valittuSiirto);
+                                    }
+                                    asema.tee_siirto(valittuSiirto, asema._siirtovuoro);
+                                    validMove = true;
+                                    break;
+                                }
+                            }
+                            if (!validMove)
+                                siirtoValittu = false;
+                        }
                     }
                 }
             }
         }
 
+        // Piirretään shakkilauta ja nappulat
         window.clear();
         sf::Color darkBrown(139, 69, 19);
         sf::Color lightBrown(222, 184, 135);
-        // Piirretään shakkilauta
         for (int y = 0; y < 8; ++y) {
             for (int x = 0; x < 8; ++x) {
                 sf::RectangleShape tile(sf::Vector2f(tileSize, tileSize));
                 tile.setPosition(x * tileSize + 50, y * tileSize + 50);
-                if ((x + y) % 2 == 0)
-                    tile.setFillColor(lightBrown);
-                else
-                    tile.setFillColor(darkBrown);
+                tile.setFillColor(((x + y) % 2 == 0) ? lightBrown : darkBrown);
                 window.draw(tile);
 
-                // Piirretään nappulat
                 int piece = asema._lauta[y][x];
                 if (pieceMap.find(piece) != pieceMap.end()) {
                     sf::Sprite sprite;
@@ -187,67 +190,106 @@ void sfml_gui(Asema& asema) {
                 }
             }
         }
-        // Piirretään mahdolliset siirrot vihreinä ympyröinä
-        for (const auto& s : mahdollisetSiirrot) {
-            if (s._a_r == valittuRivi && s._a_l == valittuLinja) {
-                sf::CircleShape circle(15);
-                circle.setFillColor(sf::Color(0, 255, 0, 150));
-                circle.setPosition(s._l_l * tileSize + 50 + 35, s._l_r * tileSize + 50 + 35);
-                window.draw(circle);
+        // Piirretään vihreät pallot ainoastaan niille siirroille, jotka lähtevät valitusta ruudusta.
+        if (siirtoValittu) {
+            for (const auto& s : mahdollisetSiirrot) {
+                if (s._a_r == valittuRivi && s._a_l == valittuLinja) {
+                    sf::CircleShape circle(15);
+                    circle.setFillColor(sf::Color(0, 255, 0, 150));
+                    circle.setPosition(s._l_l * tileSize + 50 + 35, s._l_r * tileSize + 50 + 35);
+                    window.draw(circle);
+                }
             }
         }
         window.draw(text);
         window.draw(text2);
         window.display();
+
+        // Jos ei ole pelaajan vuoro, AI laskee siirtonsa minimaxilla
+        if (asema._siirtovuoro != playerColor) {
+            vector<Siirto> legalMoves;
+            asema.anna_siirrot(legalMoves);
+            if (!legalMoves.empty()) {
+                MinimaxArvo bestMove = asema.minimaxAlphaBetaAsync(
+                    4,
+                    -std::numeric_limits<float>::infinity(),
+                    std::numeric_limits<float>::infinity(),
+                    true
+                );
+                int fromRow = bestMove._siirto._a_r;
+                int fromCol = bestMove._siirto._a_l;
+                // Tarkistetaan ylennystilanne
+                if ((asema._siirtovuoro == VALKEA && bestMove._siirto._l_r == 0 && asema._lauta[fromRow][fromCol] == wP) ||
+                    (asema._siirtovuoro == MUSTA && bestMove._siirto._l_r == 7 && asema._lauta[fromRow][fromCol] == bP))
+                {
+                    bestMove._siirto._korotettava_nappula = (asema._siirtovuoro == VALKEA ? wQ : bQ);
+                }
+                asema.tee_siirto(bestMove._siirto, asema._siirtovuoro);
+                sf::sleep(sf::seconds(0.5));
+            }
+        }
     }
 }
 
-// Tekstipohjainen käyttöliittymä komentorivillä
-void terminal_ui(Asema& asema) {
-    vector<Siirto> siirrot;
-    siirrot.reserve(100);
+// Terminaalikäyttöliittymä: Pelaaja syöttää siirtonsa manuaalisesti, kunnes on AI:n vuoro.
+void terminal_ui(Asema& asema, int playerColor) {
     while (true) {
         asema.tulosta();
-        siirrot.clear();
-        asema.anna_siirrot(siirrot);
-        MinimaxArvo minimaxTulos = asema.minimaxAlphaBetaAsync(
-            6,
-            -std::numeric_limits<float>::infinity(),
-            std::numeric_limits<float>::infinity(),
-            true  // tai false, jos et halua rinnakkaista juuritasoa
-        );
+        vector<Siirto> legalMoves;
+        asema.anna_siirrot(legalMoves);
+        if (legalMoves.empty())
+            break;
 
-        std::cout << "Minimax Arvo: " << minimaxTulos._arvo << std::endl;
-        std::cout << "Paras siirto: "
-            << "Lahto: "
-            << static_cast<char>('a' + minimaxTulos._siirto._a_l)
-            << static_cast<char>('1' + (7 - minimaxTulos._siirto._a_r))
-            << " -> Kohde: "
-            << static_cast<char>('a' + minimaxTulos._siirto._l_l)
-            << static_cast<char>('1' + (7 - minimaxTulos._siirto._l_r))
-            << std::endl;
-        cout << "Siirtoja: " << siirrot.size() << endl;
-        int lahto_rivi, lahto_linja, kohde_rivi, kohde_linja;
-        Siirto kayttajan_siirto;
-       
-        while (true) {
-            asema.kysy_siirto(asema._siirtovuoro, lahto_rivi, lahto_linja, kohde_rivi, kohde_linja);
-            kayttajan_siirto = Siirto(lahto_rivi, lahto_linja, kohde_rivi, kohde_linja);
-            bool laillinen_siirto = false;
-            for (const auto& s : siirrot) {
-                if (s == kayttajan_siirto) {
-                    laillinen_siirto = true;
-                    break;
+        if (asema._siirtovuoro == playerColor) {
+            int lahto_rivi, lahto_linja, kohde_rivi, kohde_linja;
+            Siirto kayttajan_siirto;
+            while (true) {
+                cout << "Syötä siirto muodossa: lahto_rivi lahto_linja kohde_rivi kohde_linja" << endl;
+                asema.kysy_siirto(asema._siirtovuoro, lahto_rivi, lahto_linja, kohde_rivi, kohde_linja);
+                kayttajan_siirto = Siirto(lahto_rivi, lahto_linja, kohde_rivi, kohde_linja);
+                bool laillinen_siirto = false;
+                for (const auto& s : legalMoves) {
+                    if (s == kayttajan_siirto) {
+                        laillinen_siirto = true;
+                        break;
+                    }
                 }
+                if (laillinen_siirto)
+                    break;
+                else
+                    cout << "Ei laillinen siirto! Yritä uudelleen." << endl;
             }
-            if (laillinen_siirto)
-                break;
-            else
-                cout << "Ei laillinen siirto! Yritä uudelleen." << endl;
+            // Tarkistetaan ylennystilanne
+            if ((asema._siirtovuoro == VALKEA && kohde_rivi == 0 && asema._lauta[lahto_rivi][lahto_linja] == wP) ||
+                (asema._siirtovuoro == MUSTA && kohde_rivi == 7 && asema._lauta[lahto_rivi][lahto_linja] == bP))
+            {
+                kayttajan_siirto._korotettava_nappula = (asema._siirtovuoro == VALKEA ? wQ : bQ);
+            }
+            asema.tee_siirto(kayttajan_siirto, asema._siirtovuoro);
         }
-        asema.tee_siirto(kayttajan_siirto, asema._siirtovuoro);
-        asema.anna_siirrot(siirrot);
+        else {  // AI:n vuoro
+            MinimaxArvo bestMove = asema.minimaxAlphaBetaAsync(
+                4,
+                -std::numeric_limits<float>::infinity(),
+                std::numeric_limits<float>::infinity(),
+                true
+            );
+            cout << "Minimax Arvo: " << bestMove._arvo << endl;
+            cout << "Paras siirto: Lahto: "
+                << static_cast<char>('a' + bestMove._siirto._a_l)
+                << static_cast<char>('1' + (7 - bestMove._siirto._a_r))
+                << " -> Kohde: "
+                << static_cast<char>('a' + bestMove._siirto._l_l)
+                << static_cast<char>('1' + (7 - bestMove._siirto._l_r))
+                << endl;
+            int fromRow = bestMove._siirto._a_r;
+            int fromCol = bestMove._siirto._a_l;
+            if ((asema._siirtovuoro == VALKEA && bestMove._siirto._l_r == 0 && asema._lauta[fromRow][fromCol] == wP) ||
+                (asema._siirtovuoro == MUSTA && bestMove._siirto._l_r == 7 && asema._lauta[fromRow][fromCol] == bP))
+            {
+                bestMove._siirto._korotettava_nappula = (asema._siirtovuoro == VALKEA ? wQ : bQ);
+            }
+            asema.tee_siirto(bestMove._siirto, asema._siirtovuoro);
+        }
     }
 }
-
-
